0338842bae561c846b48fe947ffef117de56cf12
Jonatan <jonatan.petur@gmail.com>
2017-04-09 23:13:33 +0200

    added improved config scripts

---
+++ usecases/menu/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export conf=~/DIT-168 
+export FREQ=10
+
+PS3='> '
+DEBUG=true
+
+xhost + > /dev/null
+
+echo "Select a configuration : " 
+
+cd configs;
+select FOLDER in *
+do
+	export conf=$PWD/$FOLDER
+	break;
+done
+cd ..
+
+$DEBUG && echo "Configuration path: $conf"
+
+echo "Select which docker-compose files to include : "
+
+select FILENAME in $(echo $(ls docker-compose.*.yml | sed 's/docker-compose.//;s/.yml//') 'CONTINUE')
+do
+	if [ $FILENAME == 'CONTINUE' ] 
+	then
+		break;	
+	fi
+	
+	FILES+=($FILENAME)
+done
+
+FILEARG=" -f docker-compose.yml "
+
+for i in "${FILES[@]}"
+do
+	FILEARG+="-f docker-compose.$i.yml " 
+done
+
+docker-compose $FILEARG up --build
